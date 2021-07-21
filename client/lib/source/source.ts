5185db527af0d1c1a1539b0a9c6debcafc9c4866
Matt Hill <matthewonthemoon@gmail.com>
2021-07-21 15:17:16 -0600

    revert

--- client/lib/source/source.ts
+++ client/lib/source/source.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs'
 import { Store } from '../store'
-import { HashMap, Update } from '../types'
+import { Update } from '../types'
 
-export interface Source<T extends HashMap> {
+export interface Source<T> {
   watch$ (store?: Store<T>): Observable<Update<T>>
 }
--- client/lib/types.ts
+++ client/lib/types.ts
@@ -3,9 +3,9 @@ import { Operation } from './json-patch-lib'
 // revise a collection of nodes.
 export type Revision = { id: number, patch: Operation[], expireId: string | null }
 // dump/replace the entire store with T
-export type Dump<T extends HashMap> = { id: number, value: T, expireId: string | null }
+export type Dump<T> = { id: number, value: T, expireId: string | null }
 
-export type Update<T extends HashMap> = Revision | Dump<T>
+export type Update<T> = Revision | Dump<T>
 
 export enum PatchOp {
   ADD = 'add',
@@ -13,20 +13,17 @@ export enum PatchOp {
   REPLACE = 'replace',
 }
 
-export interface Http<T extends HashMap> {
+export interface Http<T> {
   getRevisions (since: number): Promise<Revision[] | Dump<T>>
   getDump (): Promise<Dump<T>>
 }
 
-export interface Bootstrapper<T extends HashMap> {
+export interface Bootstrapper<T> {
   init (): Promise<DBCache<T>>
   update (cache: DBCache<T>): Promise<void>
 }
 
-export interface DBCache<T extends HashMap>{
+export interface DBCache<T extends { [key: string]: any }>{
   sequence: number,
   data: T
 }
-
-export type HashMap = { [type: string]: any }
-
