f2e0804274815ecc4da1a0d0ff5b8b9d4c01bd3d
J M <mogulslayer@gmail.com>
2022-06-03 12:27:10 -0600

    chore: convert to a potential error instead, older api

--- patch-db/src/bulk_locks.rs
+++ patch-db/src/bulk_locks.rs
@@ -120,8 +120,8 @@ where
     pub async fn set<DH: DbHandle>(&self, db_handle: &mut DH, new_value: T) -> Result<(), Error> {
         self.set_(db_handle, new_value, &[]).await
     }
-    pub async fn get<DH: DbHandle>(&self, db_handle: &mut DH) -> Result<Option<T>, Error> {
-        self.get_(db_handle, &[]).await
+    pub async fn get<DH: DbHandle>(&self, db_handle: &mut DH) -> Result<T, Error> {
+        self.get_(db_handle, &[]).await.and_then(|x| x.map(Ok).unwrap_or_else(|| serde_json::from_value(serde_json::Value::Null).map_err(Error::JSON)))
     }
 }
 impl<T> LockReceipt<T, String>
