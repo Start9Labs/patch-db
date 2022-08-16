9cccf5799176e0ee85aadbfe48f047db8477881b
waterplea <alexander@inkin.ru>
2022-08-16 17:20:55 +0300

    chore: rollback revision handling

--- client/lib/store.ts
+++ client/lib/store.ts
@@ -100,10 +100,8 @@ export class Store<T extends { [key: string]: any }> {
 
   update(update: Update<T>): void {
     if (this.isRevision(update)) {
-      // if old or known, re-trigger the latest revision
-      if (update.id <= this.cache.sequence || this.stash.get(update.id)) {
-        this.updateSequence(this.cache.sequence)
-      } else {
+      // Handle revision if new and not known
+      if (update.id > this.cache.sequence && !this.stash.get(update.id)) {
         this.handleRevision(update)
       }
     } else {
