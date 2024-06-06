7aa53249f9353162475ea347abac92abcfba5493
Matt Hill <MattDHill@users.noreply.github.com>
2024-06-06 07:30:01 -0600

    remove bootstraop and drop alias for dump (#63)

--- client/lib/types.ts
+++ client/lib/types.ts
@@ -1,12 +1,10 @@
 import { Operation } from './json-patch-lib'
 
-// revise a collection of nodes.
 export type Revision = {
   id: number
   patch: Operation<unknown>[]
 }
 
-// dump/replace the entire store with T
 export type Dump<T> = { id: number; value: T }
 
 export type Update<T> = Revision | Dump<T>
@@ -15,14 +13,4 @@ export enum PatchOp {
   ADD = 'add',
   REMOVE = 'remove',
   REPLACE = 'replace',
-}
-
-export interface Bootstrapper<T extends Record<string, any>> {
-  init(): DBCache<T>
-  update(cache: DBCache<T>): void
-}
-
-export interface DBCache<T extends Record<string, any>> {
-  sequence: number
-  data: T
 }
